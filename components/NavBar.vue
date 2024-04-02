3db3cd05010d4002b826f45771b53255f122b85e
RODRIGO LESSA <RODRIGO.LESSA@CALLMED.LOCAL>
2024-04-02 09:58:38 -0300

    add dark mode

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -25,7 +25,16 @@
             <li>
               <a href="https://github.com/rodlessa" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Github</a>
             </li>
+            <li>
+              <label class="inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" @change="darkMode" class="sr-only peer" id="darkModeToggle">
+  <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
+</label>
+
+            </li>
           </ul>
+ 
         </div>
       </div>
     </nav>
@@ -39,7 +48,25 @@ export default {
     toggleMenu() {
       const menu = document.getElementById('menu');
       menu.classList.toggle("hidden");
+    },
+    darkMode() {
+  const body = document.body;
+  const infoblock = document.getElementsByClassName("info-block");
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  
+  if (darkModeToggle.checked) {
+    body.classList.add('dark');
+    for (let i = 0; i < infoblock.length; i++) {
+      infoblock[i].classList.add('dark');
     }
+  } else {
+    body.classList.remove('dark');
+    for (let i = 0; i < infoblock.length; i++) {
+      infoblock[i].classList.remove('dark');
+    }
+  }
+}
+
   }
 }
 </script>
